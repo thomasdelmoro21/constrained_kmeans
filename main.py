72f256c3adb00628c7a47266314ff7ac0506afeb
loreb <lorebaia@gmail.com>
2023-10-03 09:07:44 +0200

    add datasets method

--- main.py
+++ main.py
@@ -37,42 +37,52 @@ def miq_kmeans(data, k):
     return objective
 
 
-DATASET = 2
+def get_datasets():
+    all_dataset = []
+
+    # XCLARA
+    data = pd.read_csv("./xclara.csv")
+    k = 3
+    plot_dataset(data)
+    all_dataset.append({"data": data, "k": k})
+
+    # HEART DISEASE PATIENTS
+    data = pd.read_csv("./heart_disease_patients.csv")
+    k = 2
+    plot_dataset(data)
+    all_dataset.append({"data": data, "k": k})
+
+    # KIVA COUNTRY PROFILE VARIABLES
+    data = pd.read_csv("./kiva_country_profile_variables.csv")
+    print(data)
+    data = data.iloc[:, 2:]
+    replacement = {'~0': 0,
+                   '-~0.0': 0,
+                   '~0.0': 0,
+                   '...': -99,
+                   '': -99}
+    data = data.replace(replacement)
+    k = 10
+    plot_dataset(data)
+    all_dataset.append({"data": data, "k": k})
+
+    # COUNTRY PROFILE VARIABLES
+    data = pd.read_csv("./country_profile_variables.csv")
+    k = 3
+    plot_dataset(data)
+    all_dataset.append({"data": data, "k": k})
+
+    return all_dataset
 
 
 def main():
     data = None
     k = None
 
-    if(DATASET == 1):
-        data = pd.read_csv("./xclara.csv")
-        plot_dataset(data)
-        k = 3
-
-    if DATASET == 2:
-        data = pd.read_csv("./heart_disease_patients.csv")
-        k = 2
-
-    if(DATASET == 10):
-        data = pd.read_csv("./kiva_country_profile_variables.csv")
-        print(data)
-        data = data.iloc[:, 2:]
-        replacement = {'~0': 0,
-                       '-~0.0': 0,
-                       '~0.0': 0,
-                       '...': -99,
-                       '': -99}
-        cleaned_data = data.replace(replacement)
-        k = 10
-
-    if (DATASET == 11):
-        data = pd.read_csv("./country_profile_variables.csv")
-        k = 3
-    
-    kmeans(data, k)
-    miq_kmeans(data, k)
-
-    plot_all()
+    for data in get_datasets():
+        kmeans(data.get("data"), data.get("k"))
+        miq_kmeans(data.get("data"), data.get("k"))
+        plot_all()
 
 
 if __name__ == '__main__':
